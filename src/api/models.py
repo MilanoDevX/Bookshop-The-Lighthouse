5759bb3de3a42a304a3be35b5c33b969597040e0
Elías Milano <87205729+MilanoDevX@users.noreply.github.com>
2025-02-26 19:01:02 +0000

    data modelling

--- src/api/models.py
+++ src/api/models.py
@@ -6,6 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
+    full_name = db.Column(db.String(200), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
@@ -15,5 +16,40 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "full_name": self.full_name
             # do not serialize the password, its a security breach
         }
+    
+
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), unique=False, nullable=False)
+    books = db.relationship("Book", back_populates="author") 
+
+    def __repr__(self):
+        return f'<Author {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }
+
+
+class Book(db.Model):
+    __tablename__ = "Books" # para dar nombre a la tabla en psql
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey("author.id")) #This is necessary because the relationship stablished in Author class.
+    author = db.relationship("Author", back_populates="books")
+    
+    def __repr__(self):
+        return f'<Book {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "author": self.author.serialize() #It serializes the author instance including his id and name (see serialize func in Author class)
+        }
+    
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Book
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
@@ -14,9 +14,22 @@ CORS(api)
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
-
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
-
     return jsonify(response_body), 200
+
+
+@api.route('/users', methods=['GET'])
+def get_all_users():
+    user_list = User.query.all()
+    serialized_users = [ item.serialize() for item in user_list ]
+    return jsonify(serialized_users), 200
+
+
+@api.route('/books', methods=['GET'])
+def get_all_books():
+    book_list = Book.query.all()
+    serialized_books = [ item.serialize() for item in book_list ]
+    return jsonify(serialized_books), 200
+
